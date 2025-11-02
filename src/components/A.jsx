8a6dfcf0144c2d3a3600d86c3e79707ac6e44d96
Akansha Tomar <159775753+tomarakansha@users.noreply.github.com>
2025-11-02 19:21:33 +0530

    Create A component that renders B component

---
+++ src/components/A.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import B from './B'
+
+const A = () => {
+  return (
+      <div>
+          <h2> A Compoment</h2>
+          <B />
+      </div>
+  )
+}
+
+export default A
